840780468938cd34500ba6a313bf9a68f00c2876
Pierre <pierrelaub@gmail.com>
2025-12-08 21:26:39 +0100

    feat: Add BigQuery environment variables and update caching TTLs to one week

--- src/config.py
+++ src/config.py
@@ -14,8 +14,8 @@ class Settings:
     BIGQUERY_DATASET = os.getenv("BIGQUERY_DATASET", "fitness_data")
 
     # Caching
-    CACHE_TTL_SESSIONS = int(os.getenv("CACHE_TTL_SESSIONS", 300))  # 5 Minuten
-    CACHE_TTL_SUMMARY = int(os.getenv("CACHE_TTL_SUMMARY", 3600))   # 1 Stunde
+    CACHE_TTL_SESSIONS = int(os.getenv("CACHE_TTL_SESSIONS", 604800))  # 1 Woche
+    CACHE_TTL_SUMMARY = int(os.getenv("CACHE_TTL_SUMMARY", 604800))   # 1 Woche
     CACHE_TTL_DETAILS = int(os.getenv("CACHE_TTL_DETAILS", 604800)) # 1 Woche
 
     # Rate Limiting
